f02fb1f44253eb1a7e75a671b497b6884f250459
Nirase <mattias.smedman@gmail.com>
2022-08-22 14:26:56 +0200

    Create SearchExtension.cs
    WIP #2
    
    Helper method that allows to search for a location based on it's name. Helpful for debugging, as it's easier to find locations based on their name than by sifting through the API. Also possibly helpful if any future search functionality is to be implemented. Could be marked as debug only in this situation but chose not to as it could technically be useful for assignment 2

---
+++ BuildVerification/SearchExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BuildVerification
+{
+    class SearchExtension
+    {
+        static DownloadRequest download = new DownloadRequest();
+
+        /// <summary>
+        /// Searches for locations based on names
+        /// </summary>
+        /// <param name="name">Name of location</param>
+        /// <returns>Id of the searched location</returns>
+        public static string GetLocationByName(string version, string parameter, string name)
+        {
+            download.GetData("https://opendata-download-metobs.smhi.se/api/version/" + version + "/parameter/" + parameter + ".xml");
+            
+            foreach(XmlNode station in download.Document.GetElementsByTagName("station"))
+            {
+                if (station["name"].InnerText == name)
+                    return station["id"].InnerText;
+            }
+            return "";
+        }
+
+    }
+}
