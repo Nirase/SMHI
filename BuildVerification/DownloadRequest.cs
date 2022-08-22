4cc2727039cd65cd4e0fe94eb9bc27a9570189ac
Nirase <mattias.smedman@gmail.com>
2022-08-22 14:23:56 +0200

    Generic method to download XML
    WIP #2
    
    Generic class that helps download XML data, might not be super helpful but helps with organizing the code in case there's a reason to make the URLs more modular, such as through a dropdown menu when choosing parameters.

---
+++ BuildVerification/DownloadRequest.cs
@@ -0,0 +1,41 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BuildVerification
+{
+    class DownloadRequest
+    {
+        public XmlDocument Document { get; private set; }
+
+        public DownloadRequest() 
+        {
+            Document = new XmlDocument();
+        }
+        public DownloadRequest(XmlDocument document)
+        {
+            this.Document = document;
+        }
+
+      
+        public XmlDocument GetData(string url)
+        {
+            try
+            {
+                Document.Load(url);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Document = null;
+                return Document;
+            }
+
+            return Document;
+
+        }
+    }
+}
