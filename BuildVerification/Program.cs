afa7689313ef2d33cd010b5d65d815d7fc976d76
Nirase <mattias.smedman@gmail.com>
2022-08-22 08:38:37 +0200

    Initial commit

---
+++ BuildVerification/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BuildVerification
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			ConnectAsync().Wait();
+
+			Console.WriteLine();
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+		}
+
+		private static async Task ConnectAsync()
+		{
+			try
+			{
+				using (var httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(10) })
+				using (HttpResponseMessage response =
+					await httpClient.GetAsync(new Uri("https://opendata.smhi.se/")))
+				{
+					Console.WriteLine(
+						response.IsSuccessStatusCode
+							? "Verification successful"
+							: $"Verification failure: {response.ReasonPhrase}");
+				}
+			}
+			catch (HttpRequestException e)
+			{
+				Console.WriteLine(e);
+			}
+		}
+	}
+}
